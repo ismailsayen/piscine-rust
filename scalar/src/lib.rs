584a217470a2e1a696b57f478b47ce2b958050b9
isayen <ismailsvn02@gmail.com>
2025-09-04 11:10:24 +0100

    first commit

--- scalar/src/lib.rs
+++ scalar/src/lib.rs
@@ -1,4 +1,4 @@
-pub fn sum(a: u16, b: u16) -> u16 {
+pub fn sum(a: u8, b: u8) -> u8 {
     a + b
 }
 
@@ -10,10 +10,10 @@ pub fn pro(a: i8, b: i8) -> i8 {
     a * b
 }
 
-pub fn quo(a: i32, b: i32) -> i32 {
+pub fn quo(a: f32, b: f32) -> f32 {
     a / b
 }
 
-pub fn rem(a: i32, b: i32) -> i32 {
+pub fn rem(a: f32, b: f32) -> f32 {
     a % b
 }
