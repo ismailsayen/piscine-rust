4a507d9e035f04c98e078ddd5a2bca97d3840267
isayen <ismailsvn02@gmail.com>
2025-09-04 12:04:52 +0100

    second exercice

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -1,5 +1,5 @@
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
-    return (f - 32.0) * (1 / 9.0 / 5.0);
+    return (f - 32.0) * (1.0 / 9.0 / 5.0);
 }
 
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
