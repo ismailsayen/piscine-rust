92b4510c56d2fc0d05827a9c6ef6642ea5ccdaf9
isayen <ismailsvn02@gmail.com>
2025-09-04 11:16:11 +0100

    correct errors

--- scalar/src/lib.rs
+++ scalar/src/lib.rs
@@ -1,19 +1,19 @@
 pub fn sum(a: u8, b: u8) -> u8 {
-    a + b
+    return a + b;
 }
 
 pub fn diff(a: i32, b: i32) -> i32 {
-    a - b
+    return a - b;
 }
 
 pub fn pro(a: i8, b: i8) -> i8 {
-    a * b
+    return a * b;
 }
 
 pub fn quo(a: f32, b: f32) -> f32 {
-    a / b
+    return a / b;
 }
 
 pub fn rem(a: f32, b: f32) -> f32 {
-    a % b
+    return a % b;
 }
