dcbd6272a1ab489ebaa115e70da37457db47f404
Ismail Sayen <ismailsvn02@gmail.com>
2025-09-04 17:53:48 +0100

    matrix

--- matrix_transposition/src/lib.rs
+++ matrix_transposition/src/lib.rs
@@ -1,8 +1,5 @@
-#[derive(Debug)]
+#[derive(Debug,PartialEq, Eq)]
 pub struct Matrix(pub (i32, i32), pub (i32, i32));
 pub fn transpose(m: Matrix) -> Matrix {
-    
-   println!("{:?}",m.0.1);
-//    m
     Matrix((m.0.0,m.1.0), (m.0.1,m.1.1))
 }
