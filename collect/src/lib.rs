be921e7d70ded48989af95993eb982559e011e69
Ismail Sayen <ismailsvn02@gmail.com>
2025-09-07 13:08:51 +0100

    bubble sort

--- collect/src/lib.rs
+++ collect/src/lib.rs
@@ -1,8 +1,8 @@
 pub fn bubble_sort(arr: &mut [i32]) {
     for i in 0..arr.len() {
-        for j in 0..arr.len()-i - 1 {
+        for j in 0..arr.len()-i-1 {
             if arr[j] > arr[j + 1] {
-                arr.swap(i, j+1);
+                arr.swap(j, j+1);
             }
         }
     }
