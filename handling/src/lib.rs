708a2d3da008a14113ce48f139290b003c1613d5
Ismail Sayen <ismailsvn02@gmail.com>
2025-09-11 15:35:45 +0100

    handling

--- handling/src/lib.rs
+++ handling/src/lib.rs
@@ -1,8 +1,13 @@
+use std::fs::OpenOptions;
 use std::io::Write;
 use std::path::Path;
-use std::fs::OpenOptions;
 
 pub fn open_or_create<P: AsRef<Path>>(path: &P, content: &str) {
-    let  file=OpenOptions::new().read(true).write(true).create(true).open(path);
-   let _= file.unwrap().write(content.as_bytes());
+    let file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .append(true)
+        .open(path);
+    let _ = file.unwrap().write(content.as_bytes());
 }
