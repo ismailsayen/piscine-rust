9b51cc7d32db998a559653d57d3653b7a808fadb
Ismail Sayen <ismailsvn02@gmail.com>
2025-09-13 15:55:33 +0100

    pangram

--- pangram/src/lib.rs
+++ pangram/src/lib.rs
@@ -3,14 +3,17 @@ use std::collections::HashMap;
 pub fn is_pangram(s: &str) -> bool {
     println!("{s}");
     let mut chars_in_s: HashMap<char, u32> = HashMap::new();
-    for ch in s.chars(){
-        if ch.is_ascii_alphabetic(){
-            chars_in_s.entry(ch).and_modify(|count|{*count+=1}).or_insert(1);
-        }else{
+    for ch in s.chars() {
+        if ch.is_ascii_alphabetic() {
+            chars_in_s
+                .entry(ch.to_ascii_lowercase())
+                .and_modify(|count| *count += 1)
+                .or_insert(1);
+        } else {
             continue;
         }
     }
-    if chars_in_s.len()==26{
+    if chars_in_s.len() == 26 {
         return true;
     }
     false
