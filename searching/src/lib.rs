db38abf2d9bb7df5f64a92db5ba3fc6a8eaca5a7
Ismail Sayen <ismailsvn02@gmail.com>
2025-09-13 13:14:41 +0100

    addddddddd

--- searching/src/lib.rs
+++ searching/src/lib.rs
@@ -1,8 +1,8 @@
 pub fn search(array: &[i32], key: i32) -> Option<usize> {
     let mut ind:Option<usize>=None;
     for i in 0..array.len(){
-        match key {
-            8i32=>ind=Some(i),
+        match array[i] {
+            key=>ind=Some(i),
             _=>continue,
         }
     }
