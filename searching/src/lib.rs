4c8d6add8809c8fbb257e0e978cf6170434bb862
Ismail Sayen <ismailsvn02@gmail.com>
2025-09-13 13:42:54 +0100

    aaaaaaq

--- searching/src/lib.rs
+++ searching/src/lib.rs
@@ -1,12 +1,13 @@
 pub fn search(array: &[i32], key: i32) -> Option<usize> {
-    println!("{:?} {}",array,key);
+   
     let mut ind: Option<usize> = None;
-    for i in (0..array.len()).rev() {
+    for i in 0..array.len() {
         ind = if array[i as usize] == key {
             Some(i as usize)
         }else{
             continue;
-        }
+        };
+       
     }
     ind
 }
--- searching/src/main.rs
+++ searching/src/main.rs
@@ -1,7 +1,7 @@
 use searching::*;
 
 fn main() {
-    let ar = [1, 3, 4, 6, 8, 9, 11];
+    let ar = [1, 3, 4, 1, 8, 9, 11];
     let f = search(&ar ,1);
     println!(
         "the element 8 is last in the position {:?} in the array {:?}",
