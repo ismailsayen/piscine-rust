7d5757ad184fa98093db2b352fca4807461e4559
Ismail Sayen <ismailsvn02@gmail.com>
2025-09-13 12:03:50 +0100

    addddddddd

--- expected_variable/src/lib.rs
+++ expected_variable/src/lib.rs
@@ -1,10 +1,10 @@
 use convert_case::{Case, Casing};
 use edit_distance::*;
 pub fn expected_variable(str1: &str, str2: &str) -> Option<String> {
-    if str1.to_lowercase() == str1.to_lowercase().to_case(Case::Camel)
-        || str1.to_lowercase() == str1.to_lowercase().to_case(Case::Snake)
+    if str1.to_ascii_lowercase() == str1.to_ascii_lowercase().to_case(Case::Camel)
+        || str1.to_ascii_lowercase() == str1.to_ascii_lowercase().to_case(Case::Snake)
     {
-        let n_change = edit_distance(&str1.to_lowercase(), &str2.to_lowercase());
+        let n_change = edit_distance(&str1.to_ascii_lowercase(), &str2.to_ascii_lowercase());
         let len = str2.len();
 
         if len == 0 {
