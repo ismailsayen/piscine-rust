9a8d287df7ceb82ca4b39dd2aadbe3f064cc1d3d
Ismail Sayen <ismailsvn02@gmail.com>
2025-09-13 11:52:29 +0100

    expected

---
+++ expected_variable/src/lib.rs
@@ -0,0 +1,24 @@
+use convert_case::{Case, Casing};
+use edit_distance::*;
+pub fn expected_variable(str1: &str, str2: &str) -> Option<String> {
+    if str1.to_lowercase() == str1.to_lowercase().to_case(Case::Camel)
+        || str1.to_lowercase() == str1.to_lowercase().to_case(Case::Snake)
+    {
+        let n_change = edit_distance(&str1.to_lowercase(), &str2.to_lowercase());
+        let len = str2.len();
+
+        if len == 0 {
+            return None;
+        }
+
+        let similarity = ((len as f64 - n_change as f64) / len as f64) * 100.0;
+        // println!("{similarity} {n_change} {len}");
+        if similarity >= 50.0 {
+            return Some(format!("{}%", similarity.floor()));
+        }
+    } else {
+        return None;
+    }
+
+    None
+}
