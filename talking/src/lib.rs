10770f75231e6b3d1c6b74ad71e54a5727b1f989
Ismail Sayen <ismailsvn02@gmail.com>
2025-09-13 17:23:08 +0100

    tzlking

---
+++ talking/src/lib.rs
@@ -0,0 +1,30 @@
+pub fn talking(text: &str) -> &str {
+   if text.trim().chars().count()==0{
+      return "Just say something!";
+   }
+    match text.trim().chars().last().unwrap() {
+        '!'=>{
+         if is_yelling(text){
+            return "There is no need to yell, calm down!";
+         }
+          return "Interesting";
+        },
+        '?'=>{
+         if is_yelling(text){
+            return "Quiet, I am thinking!";
+         }
+          return "Sure.";
+        },
+        _=> "Interesting",
+    }
+}
+
+pub fn is_yelling(text: &str) -> bool {
+    for ch in text.chars() {
+        if ch.is_ascii_alphabetic() && ch.is_ascii_lowercase() {
+            return false;
+        }
+    }
+
+    true
+}
