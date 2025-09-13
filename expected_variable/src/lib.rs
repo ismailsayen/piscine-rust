dcd51c3e6a3a720c04862bc6339df32d640faac8
Ismail Sayen <ismailsvn02@gmail.com>
2025-09-13 12:04:38 +0100

    addddddddd

--- expected_variable/src/lib.rs
+++ expected_variable/src/lib.rs
@@ -1,15 +1,14 @@
 use convert_case::{Case, Casing};
 use edit_distance::*;
 pub fn expected_variable(str1: &str, str2: &str) -> Option<String> {
+
+    println!("{str1} {str2}");
     if str1.to_ascii_lowercase() == str1.to_ascii_lowercase().to_case(Case::Camel)
         || str1.to_ascii_lowercase() == str1.to_ascii_lowercase().to_case(Case::Snake)
     {
         let n_change = edit_distance(&str1.to_ascii_lowercase(), &str2.to_ascii_lowercase());
         let len = str2.len();
 
-        if len == 0 {
-            return None;
-        }
 
         let similarity = ((len as f64 - n_change as f64) / len as f64) * 100.0;
         if similarity >= 50.0 {
