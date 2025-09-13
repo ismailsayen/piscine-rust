abb240fc405c162c957ea45c9801391ad4640537
Ismail Sayen <ismailsvn02@gmail.com>
2025-09-13 17:53:57 +0100

    tzlking

--- talking/src/lib.rs
+++ talking/src/lib.rs
@@ -1,5 +1,5 @@
 pub fn talking(text: &str) -> &str {
-   println!("{:?}",text);
+    // println!("{:?}",text);
     if text.trim().chars().count() == 0 {
         return "Just say something!";
     }
@@ -22,7 +22,10 @@ pub fn talking(text: &str) -> &str {
 
 pub fn is_yelling(text: &str) -> bool {
     for ch in text.chars() {
-        if (ch.is_ascii_alphanumeric() && ch.is_ascii_lowercase()) || ch.is_numeric() {
+        if ch.is_numeric() {
+            continue;
+        }
+        if ch.is_ascii_alphanumeric() && ch.is_ascii_lowercase() {
             return false;
         }
     }
--- talking/src/main.rs
+++ talking/src/main.rs
@@ -6,4 +6,5 @@ fn main() {
     println!("{:?}", talking("WHAT'S GOING ON?"));
     println!("{:?}", talking("7?"));
     println!("{:?}", talking("WHAT'S GOING ON?"));
+    println!("{:?}",talking("JUST DO IT!"));
 }
