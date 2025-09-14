890cf4b424b10c237ea37de01ad78197530fa100
Ismail Sayen <ismailsvn02@gmail.com>
2025-09-14 12:45:30 +0100

    cipher

---
+++ scytale_cipher/src/lib.rs
@@ -0,0 +1,25 @@
+pub fn scytale_cipher(message: &str, i: usize) -> String {
+    let mut cipher: Vec<Vec<char>> = vec![];
+    let mut word: Vec<char> = vec![];
+    let mut res = String::new();
+    for (index, ch) in message.chars().enumerate() {
+        word.push(ch);
+        if word.len() < i && index + 1 == message.len() {
+            while word.len() < i {
+                word.push(' ');
+            }
+            cipher.push(word.clone());
+            break;
+        }
+        if word.len() == i {
+            cipher.push(word.clone());
+            word.clear();
+        }
+    }
+    for id in 0..i {
+        res.push(cipher[0][id]);
+        res.push(cipher[1][id]);
+    }
+
+    (&res.trim()).to_string()
+}
