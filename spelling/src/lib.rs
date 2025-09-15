5850ed360aa6e1bf6c9d7c883b11d8edbd74ee35
Ismail Sayen <ismailsvn02@gmail.com>
2025-09-15 17:42:15 +0100

    speellling

--- spelling/src/lib.rs
+++ spelling/src/lib.rs
@@ -59,7 +59,7 @@ pub fn units(n: u64) -> String {
         3 => "three".to_string(),
         4 => "four".to_string(),
         5 => "five".to_string(),
-        6 => "sex".to_string(),
+        6 => "six".to_string(),
         7 => "seven".to_string(),
         8 => "eight".to_string(),
         9 => "nine".to_string(),
