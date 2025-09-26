fd94924fb373abc344251e137adf27b85d35250b
ismailsayen <ismailsvn02@gmail.com>
2025-09-26 12:23:28 +0100

    pussssh

---
+++ delete_prefix/src/lib.rs
@@ -0,0 +1,17 @@
+pub fn delete_prefix<'a>(prefix: &'a str, s: &'a str) -> Option<&'a str> {
+    if prefix.len() > s.len() {
+        return None;
+    }
+    let mut pr = "".to_string();
+    for (i, ch) in s.chars().enumerate() {
+        if i == prefix.len() {
+            break;
+        }
+        pr.push(ch);
+    }
+    
+    if prefix != pr {
+        return None;
+    }
+   s.strip_prefix(prefix)
+}
