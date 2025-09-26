b6eae05d7e12ed353677fe5a287e5665ef480319
ismailsayen <ismailsvn02@gmail.com>
2025-09-26 10:40:58 +0100

    pussssh

--- matrix_mult/src/lib.rs
+++ matrix_mult/src/lib.rs
@@ -1,12 +1,14 @@
-use std::fmt::Debug;
 
-pub use matrix::*;
-use std::ops::Mul;
+pub use lalgebra_scalar::*;
+use std::ops::{Add, Mul};
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct Matrix<T>(pub Vec<Vec<T>>);
 
-impl<T: Clone + Debug> Matrix<T> {
+impl<T: Scalar<Item = T> + Clone > Matrix<T> {
+    pub fn new() -> Matrix<T> {
+        Matrix(vec![])
+    }
     pub fn number_of_cols(&self) -> usize {
         self.0.len()
     }
@@ -28,7 +30,32 @@ impl<T: Clone + Debug> Matrix<T> {
     }
 }
 
-impl<T:Copy + Mul<Output = T>> Mul for Matrix<T> {
-   type Output = Option<Matrix<T>>;
- 
+impl<T: Scalar<Item = T> + Mul<Output = T> + Copy + Add<Output = T>> Mul for Matrix<T> {
+    type Output = Option<Matrix<T>>;
+    fn mul(self, other: Self) -> Self::Output {
+        if self.number_of_cols() != other.number_of_rows() {
+            return None;
+        }
+
+        let mut matrix = Matrix::new();
+
+        for i in 0..self.number_of_rows() {
+            let mut row_matrix = vec![];
+
+            for j in 0..self.number_of_cols() {
+                let mut sum = T::zero();
+
+                let row = self.row(i);
+                let col = other.col(j);
+                for k in 0..self.number_of_cols() {
+                    sum = sum + (row[k] * col[k]);
+                }
+
+                row_matrix.push(sum);
+            }
+            matrix.0.push(row_matrix);
+        }
+
+        Some(matrix)
+    }
 }
