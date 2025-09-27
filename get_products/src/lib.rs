ec7014ebe81af41f47dbcdfeede01fcc817659de
ismailsayen <ismailsvn02@gmail.com>
2025-09-27 16:39:30 +0100

    formating

--- get_products/src/lib.rs
+++ get_products/src/lib.rs
@@ -1,4 +1,7 @@
 pub fn get_products(arr: Vec<usize>) -> Vec<usize> {
+    if arr.len()<=1{
+        return vec![];
+    }
     let mut  res: Vec<usize> = vec![];
     for ele in arr.clone(){
         let prd=arr.clone().iter().filter(|e| **e!=ele).cloned().collect::<Vec<usize>>().iter().product::<usize>();
