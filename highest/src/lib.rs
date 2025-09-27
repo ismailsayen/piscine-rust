222a3af6d9f4c1c8f2b78b12f91c43570376d622
ismailsayen <ismailsvn02@gmail.com>
2025-09-27 17:07:27 +0100

    formating

--- highest/src/lib.rs
+++ highest/src/lib.rs
@@ -1,4 +1,4 @@
-#[derive(Debug,Clone)]
+#[derive(Debug, Clone)]
 pub struct Numbers<'a> {
     numbers: &'a [u32],
 }
@@ -13,16 +13,29 @@ impl<'a> Numbers<'a> {
     }
 
     pub fn latest(&self) -> Option<u32> {
-        self.numbers.to_vec().into_iter().min()
+        //  self.numbers.last().copied()
+        if self.numbers.len() > 0 {
+            Some(self.numbers[self.numbers.len() - 1])
+        } else {
+            None
+        }
     }
 
     pub fn highest(&self) -> Option<u32> {
-        self.numbers.to_vec().into_iter().max()
+        // self.numbers.iter().max().copied()
+        let mut arr = self.numbers.to_vec();
+        arr.sort();
+        if arr.len() > 0 {
+            Some(arr[arr.len() - 1])
+        } else {
+            None
+        }
     }
 
     pub fn highest_three(&self) -> Vec<u32> {
         let mut sorted_numbers: Vec<u32> = self.numbers.iter().copied().collect();
         sorted_numbers.sort_by(|a, b| b.cmp(a));
         sorted_numbers.into_iter().take(3).collect()
+
     }
 }
